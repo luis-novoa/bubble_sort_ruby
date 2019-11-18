31a9416b807435804801599d51dfb1cb0bab2f11
RoodzFernando <fleurantrfernando@gmail.com>
2019-11-18 15:41:39 -0500

    Trying the code for swaping the elements

--- bubblesortby.rb
+++ bubblesortby.rb
@@ -1,3 +1,27 @@
 def bubblesort_by(array)
-  
+  result = array
+#   loop do
+#     swap = 0
+#     for i in 0...result.length
+#     #   if result[i] > result[i + 1] 
+#     #     result[i], result[i+1] = result[i+1], result[i]
+#     #     swap+=1
+#     #   end
+#         puts result[i] <=> result[i+1]
+#     end
+#     break if swap == 0
+#   end
+#   return result
+    result.each do |item|
+       puts item <=> item
+    end
 end
+
+
+bubblesort_by(["hi","hello","hey"])
+
+# bubblesort_by(["hi","hello","hey"]) do |left,right|
+#   left.length - right.length
+#  end
+#  ["hi", "hey", "hello"]
+
