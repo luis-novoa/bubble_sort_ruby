c7ebd2b7c8ed1d7fe20afb2ef03d00817a689ce5
Luis Novoa <luis.matte.novoa@gmail.com>
2019-11-18 17:46:22 -0400

    Applies all rubocop fixes

--- bubblesortby.rb
+++ bubblesortby.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 def bubblesort_by(array)
   result = array
   loop do
     swap = 0
-    for i in 0...result.length-1
-      diff = yield(result[i], result[i+1])
-      if diff > 0
-        result[i], result[i+1] = result[i+1], result[i]
-        swap+=1
+    result.each_with_index do |_, i|
+      break if result[i + 1].nil?
+
+      if yield(result[i], result[i + 1]).positive?
+        result[i], result[i + 1] = result[i + 1], result[i]
+        swap += 1
       end
     end
-    break if swap == 0
+    break if swap.zero?
   end
   puts result
 end
 
-bubblesort_by(["hi","hello","hey"]) do |left,right|
+bubblesort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
-
