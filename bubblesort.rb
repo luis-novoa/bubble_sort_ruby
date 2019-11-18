971bd32bb3bd8e2ee14221b3c8dbd76506e5ef63
Luis Novoa <luis.matte.novoa@gmail.com>
2019-11-18 13:43:39 -0400

    Adds the swap control variable

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,13 +1,15 @@
 def run(array)
   result = array
+  swap = 0
   for i in 0...result.length
     if result[i+1] == nil
       break
     elsif result[i] > result[i+1] 
       result[i], result[i+1] = result[i+1], result[i]
+      swap += 1
     end
   end
-  return result
+  return result, swap
 end
 
-puts bubblesort([2, 1, 4, 6, 8, 0, 10])
+puts run([2, 1, 4, 6, 8, 0, 10])
